53749cde33b3b922277d8491cc55ce16c417110b
Eduardo735 <gentlelife735@gmail.com>
2025-08-09 10:03:28 -0600

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://test-web-frontend-oisq.vercel.app', // tu dominio frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // si envías cookies o Authorization headers
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
